cbda0da406515525d70b073a28cd3612d5e4ab17
Vitor Gonçalves <vitor@innovativetecnologia.com.br>
2020-10-15 14:14:49 -0300

    Criando ToDos

--- todos.js
+++ todos.js
@@ -1,1 +1,33 @@
-alert('Funcionou');
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+var toDos = [
+    'Fazer Café',
+    'Estudar Javascript',
+    'Acessar comunidade da Rocketseat'
+];
+
+function renderToDos() {
+    listElement.innerHTML = '';
+
+    for (toDo of toDos) {
+        var toDoElement = document.createElement('li');
+        var toDoText = document.createTextNode(toDo);
+
+        toDoElement.appendChild(toDoText);
+        listElement.appendChild(toDoElement);
+    }
+}
+
+renderToDos();
+
+function addToDo() {
+    var toDotext = inputElement.value;
+
+    toDos.push(toDotext);
+    inputElement.value = '';
+    renderToDos();
+}
+
+buttonElement.onclick= addToDo;
